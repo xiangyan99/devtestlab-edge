05ae13f424349fa0a84d18b5f5420f95c0178598
Markus Heiliger <markus.heiliger@dash-two.de>
2018-01-30 11:39:15 -0800

    improved gateway setup and configuation

--- factory/azuredeploy_openvpn.sh
+++ factory/azuredeploy_openvpn.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 USERPASSWORD=$1
-CLIENTPOOLPREFIX=$2
+LABSUBNETPREFIX=$2
+CLIENTPOOLPREFIX=$3
 
 apt-get update
 apt-get install sqlite3
@@ -20,15 +21,21 @@ PUBLICIP=$(curl -s ifconfig.me)
 PROCESSORS=$(grep -c ^processor /proc/cpuinfo)
 DNSZONE="$(dnsdomainname)"
 IFS='/' read -ra CLIENTPOOLPREFIXSPLIT <<< "$CLIENTPOOLPREFIX"
+RNDPASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
 cd /usr/local/openvpn_as/scripts
 ./confdba -mk "aui.eula_version" -v "2"
+./confdba -mk "auth.module.type" -v "local"
 ./confdba -mk "host.name" -v "$PUBLICIP" 
+./confdba -mk "vpn.general.osi_layer" -v "3"
 ./confdba -mk "vpn.client.config_text" -v "cipher AES-256-CBC"
 ./confdba -mk "vpn.server.config_text" -v "cipher AES-256-CBC"
 ./confdba -mk "vpn.server.daemon.tcp.n_daemons" -v "$PROCESSORS"   
 ./confdba -mk "vpn.server.dhcp_option.domain" -v "$DNSZONE"
 ./confdba -mk "vpn.server.group_pool.0" -v "$CLIENTPOOLPREFIX"
+./confdba -mk "vpn.server.routing.gateway_access" -v "true"
+./confdba -mk "vpn.server.routing.private_access" -v "route"
+./confdba -mk "vpn.server.routing.private_network.0" -v "$LABSUBNETPREFIX"
 ./confdba -mk "vpn.daemon.0.client.network" -v "${CLIENTPOOLPREFIXSPLIT[0]}"
 ./confdba -mk "vpn.daemon.0.client.netmask_bits" -v "${CLIENTPOOLPREFIXSPLIT[1]}"
 ./confdba -mk "cs.cws_ui_offer.android" -v "false"
@@ -39,6 +46,11 @@ cd /usr/local/openvpn_as/scripts
 ./confdba -mk "cs.cws_ui_offer.server_locked" -v "false"
 ./confdba -mk "cs.cws_ui_offer.user_locked" -v "false"
 ./confdba -mk "cs.cws_ui_offer.win" -v "false"
+
+./sacli --user openwrt --key "type" --value "user_connect" UserPropPut
+./sacli --user openwrt --new_pass "$RNDPASSWORD" SetLocalPassword
+./sacli --user openwrt --key "prop_autologin" --value "true" UserPropPut
+
 cd ~
 
 #restart OpenVPN AS service
